2681438067acb1eb56ef511d7ecc9433c2c9aba0
Amaury Denny <plentifisas@gmail.com>
2024-11-12 10:37:07 +0100

    Last version

---
+++ Core Curriculum/Rank-00/Libft/ft_strlcpy.c
@@ -0,0 +1,6 @@
+#include <stddef.h>
+
+char	*strlcpy(char *string1, const char *string2, size_t count)
+{
+
+}
--- Core Curriculum/Rank-00/Libft/libft.h
+++ Core Curriculum/Rank-00/Libft/libft.h
@@ -1,17 +1,19 @@
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <stddef.h>
+# include <stddef.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int ft_isprint(int c);
-int ft_strlen(char *str);
-void *ft_memset(void *ptr, int value, size_t num);
-void ft_bzero(void* s, size_t n);
-void *ft_memcpy(void * __restrict__ dest, const void * __restrict__ src, size_t count);
-void ft_memmove(void *dest, const void *src, size_t count);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_strlen(char *str);
+void	*ft_memset(void *ptr, int value, size_t num);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *__restrict__ dest, const void *__restrict__ src,
+			size_t count);
+void	ft_memmove(void *dest, const void *src, size_t count);
+char	*strlcpy(char *string1, const char *string2, size_t count);
 
 #endif
