c80110a8a96595b2e42f2f3aae7576accbdf79ce
Amaury Denny <plentifisas@gmail.com>
2024-11-12 13:54:18 +0100

    Adjust

--- Core Curriculum/Rank-00/Libft/ft_strlcpy.c
+++ Core Curriculum/Rank-00/Libft/ft_strlcpy.c
@@ -8,10 +8,10 @@ size_t	strlcpy(char *string1, const char *string2, size_t count)
 		string1[c] = string2[c];
 		c++;
 	}
-	string1[c] = '\0';
 	while (string2[c])
 		c++;
-	if (count == 0)
-		return (strlen(string2));
+    if (count == 0)
+		return (c);
+    string1[c] = '\0';
 	return (c);
 }
