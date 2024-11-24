824fcd6d8bd0173efe25c47b2d4b68af26b6ed57
Amaury Denny <plentifisas@gmail.com>
2024-11-24 14:55:24 +0100

    Norme

--- Core Curriculum/Rank-01/ft_printf/ft_printf.c
+++ Core Curriculum/Rank-01/ft_printf/ft_printf.c
@@ -5,93 +5,80 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: adenny <adenny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/23 22:01:29 by adenny            #+#    #+#             */
-/*   Updated: 2024/11/24 22:01:29 by adenny           ###   ########.fr       */
+/*   Created: 2024/11/24 14:47:03 by adenny            #+#    #+#             */
+/*   Updated: 2024/11/24 14:52:18 by adenny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int safe_write(int fd, const void *buf, size_t count)
+static int	safe_write(int fd, const void *buf, size_t count)
 {
-    ssize_t ret;
+	ssize_t	ret;
 
-    ret = write(fd, buf, count);
-    if (ret == -1)
-        return -1;
-    return (int)ret;
+	ret = write(fd, buf, count);
+	if (ret == -1)
+		return (-1);
+	return ((int)ret);
 }
 
-static int valid_format(char c)
+static int	valid_format(char c)
 {
-    return (c == 'c' || c == 's' || c == 'p' || c == 'd' || c == 'i' || 
-            c == 'u' || c == 'x' || c == 'X' || c == '%');
+	return (c == 'c' || c == 's' || c == 'p' || c == 'd' || c == 'i' || c == 'u'
+		|| c == 'x' || c == 'X' || c == '%');
 }
 
-static int print_format(char specifier, va_list ap)
+static int	print_format(char specifier, va_list ap)
 {
-    if (specifier == 'c')
-        return print_char(va_arg(ap, int));
-    else if (specifier == 's')
-        return print_str(va_arg(ap, char *));
-    else if (specifier == 'p')
-        return print_pointer(va_arg(ap, void *));
-    else if (specifier == 'd' || specifier == 'i')
-        return print_digit(va_arg(ap, int), 10);
-    else if (specifier == 'u')
-        return print_unsigned_digit(va_arg(ap, unsigned int), 10, 0);
-    else if (specifier == 'x')
-        return print_unsigned_digit(va_arg(ap, unsigned int), 16, 0);
-    else if (specifier == 'X')
-        return print_unsigned_digit(va_arg(ap, unsigned int), 16, 1);
-    else if (specifier == '%')
-        return safe_write(1, "%", 1);
-    return -1;
+	if (specifier == 'c')
+		return (print_char(va_arg(ap, int)));
+	else if (specifier == 's')
+		return (print_str(va_arg(ap, char *)));
+	else if (specifier == 'p')
+		return (print_pointer(va_arg(ap, void *)));
+	else if (specifier == 'd' || specifier == 'i')
+		return (print_digit(va_arg(ap, int), 10));
+	else if (specifier == 'u')
+		return (print_unsigned_digit(va_arg(ap, unsigned int), 10, 0));
+	else if (specifier == 'x')
+		return (print_unsigned_digit(va_arg(ap, unsigned int), 16, 0));
+	else if (specifier == 'X')
+		return (print_unsigned_digit(va_arg(ap, unsigned int), 16, 1));
+	else if (specifier == '%')
+		return (safe_write(1, "%", 1));
+	return (-1);
 }
 
-int ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...)
 {
-    va_list ap;
-    int count;
-    int temp;
+	va_list	ap;
+	int		count;
+	int		temp;
 
-    count = 0;
-    va_start(ap, format);
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == '\0')
-            {
-                va_end(ap);
-                return -1; // Handle edge case: dangling '%' at the end
-            }
-            if (!valid_format(*format))
-            {
-                va_end(ap);
-                return -1; // Handle invalid format specifier
-            }
-            temp = print_format(*format, ap);
-            if (temp == -1)
-            {
-                va_end(ap);
-                return -1;
-            }
-            count += temp;
-        }
-        else
-        {
-            temp = safe_write(1, format, 1);
-            if (temp == -1)
-            {
-                va_end(ap);
-                return -1;
-            }
-            count += temp;
-        }
-        format++;
-    }
-    va_end(ap);
-    return count;
+	count = 0;
+	va_start(ap, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format && valid_format(*format))
+				temp = print_format(*format, ap);
+			else
+				temp = -1;
+		}
+		else
+		{
+			temp = safe_write(1, format, 1);
+		}
+		if (temp == -1)
+		{
+			va_end(ap);
+			return (-1);
+		}
+		count += temp;
+		format++;
+	}
+	va_end(ap);
+	return (count);
 }
--- Core Curriculum/Rank-01/ft_printf/ft_printf_utils.c
+++ Core Curriculum/Rank-01/ft_printf/ft_printf_utils.c
@@ -5,110 +5,117 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: adenny <adenny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/23 22:01:16 by adenny            #+#    #+#             */
-/*   Updated: 2024/11/24 22:01:18 by adenny           ###   ########.fr       */
+/*   Created: 2024/11/24 14:46:56 by adenny            #+#    #+#             */
+/*   Updated: 2024/11/24 14:54:07 by adenny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int safe_write(int fd, const void *buf, size_t count)
+static int	safe_write(int fd, const void *buf, size_t count)
 {
-    ssize_t ret;
+	ssize_t	ret;
 
-    ret = write(fd, buf, count);
-    if (ret == -1)
-        return -1;
-    return (int)ret;
+	ret = write(fd, buf, count);
+	if (ret == -1)
+		return (-1);
+	return ((int)ret);
 }
 
-int print_char(int c)
+int	print_char(int c)
 {
-    return safe_write(1, &c, 1);
+	return (safe_write(1, &c, 1));
 }
 
-int print_str(char *str)
+int	print_str(char *str)
 {
-    int count;
-    int written;
+	int	count;
+	int	written;
 
-    count = 0;
-    if (!str)
-        str = "(null)";
-    while (*str)
-    {
-        written = safe_write(1, str, 1);
-        if (written == -1)
-            return -1;
-        count += written;
-        str++;
-    }
-    return count;
+	count = 0;
+	if (!str)
+		str = "(null)";
+	while (*str)
+	{
+		written = safe_write(1, str, 1);
+		if (written == -1)
+			return (-1);
+		count += written;
+		str++;
+	}
+	return (count);
 }
 
-int print_digit(long n, int base)
+int	print_digit(long n, int base)
 {
-    char buffer[65];
-    int i;
-    int count;
-    char symbols[] = "0123456789abcdef";
+	char	buffer[65];
+	int		i;
+	int		count;
+	char	symbols[] = "0123456789abcdef";
 
-    i = 64;
-    count = 0;
-    if (n < 0)
-    {
-        if (safe_write(1, "-", 1) == -1)
-            return -1;
-        count++;
-        n = -n;
-    }
-    buffer[i--] = '\0';
-    do
-    {
-        buffer[i--] = symbols[n % base];
-        n /= base;
-    } while (n > 0);
-    if (safe_write(1, &buffer[i + 1], 64 - i - 1) == -1)
-        return -1;
-    count += (64 - i - 1);
-    return count;
+	i = 64;
+	count = 0;
+	if (n < 0)
+	{
+		if (safe_write(1, "-", 1) == -1)
+			return (-1);
+		count++;
+		n = -n;
+	}
+	buffer[i--] = '\0';
+	while (n > 0)
+	{
+		buffer[i--] = symbols[n % base];
+		n /= base;
+	}
+	if (i == 64)
+	{
+		buffer[i--] = '0';
+	}
+	if (safe_write(1, &buffer[i + 1], 64 - i - 1) == -1)
+		return (-1);
+	count += (64 - i - 1);
+	return (count);
 }
 
-int print_unsigned_digit(unsigned long n, int base, int uppercase)
+int	print_unsigned_digit(unsigned long n, int base, int uppercase)
 {
-    char buffer[65];
-    int i;
-    const char *symbols;
+	char		buffer[65];
+	int			i;
+	const char	*symbols;
 
-    if (uppercase)
-        symbols = "0123456789ABCDEF";
-    else
-        symbols = "0123456789abcdef";
-
-    i = 64;
-    buffer[i--] = '\0';
-    do
-    {
-        buffer[i--] = symbols[n % base];
-        n /= base;
-    } while (n > 0);
-    if (safe_write(1, &buffer[i + 1], 64 - i - 1) == -1)
-        return -1;
-    return (64 - i - 1);
+	if (uppercase)
+		symbols = "0123456789ABCDEF";
+	else
+		symbols = "0123456789abcdef";
+	i = 64;
+	buffer[i--] = '\0';
+	while (n > 0)
+	{
+		buffer[i--] = symbols[n % base];
+		n /= base;
+	}
+	if (i == 64)
+	{
+		buffer[i--] = '0';
+	}
+	if (safe_write(1, &buffer[i + 1], 64 - i - 1) == -1)
+		return (-1);
+	return (64 - i - 1);
 }
 
-int print_pointer(void *ptr)
+int	print_pointer(void *ptr)
 {
-    unsigned long address;
+	unsigned long	address;
 
-    address = (unsigned long)ptr;
-    if (!ptr)
-    {
-        if (safe_write(1, "0x0", 3) == -1)
-            return -1;
-        return 3;
-    }
-    if (safe_write(1, "0x", 2) == -1)
-        return -1;
-    return (2 + print_unsigned_digit(address, 16, 0));
+	address = (unsigned long)ptr;
+	if (!ptr)
+	{
+		if (safe_write(1, "0x0", 3) == -1)
+			return (-1);
+		return (3);
+	}
+	if (safe_write(1, "0x", 2) == -1)
+		return (-1);
+	return (2 + print_unsigned_digit(address, 16, 0));
 }
