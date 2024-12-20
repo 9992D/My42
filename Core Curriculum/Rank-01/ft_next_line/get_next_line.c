8a95839d74f12f51ebfda4f4f51759c5d31887e3
Amaury Denny <amaury.denny@icloud.com>
2024-12-20 03:25:10 +0100

    Test

--- Core Curriculum/Rank-01/ft_next_line/get_next_line.c
+++ Core Curriculum/Rank-01/ft_next_line/get_next_line.c
@@ -6,30 +6,13 @@
 /*   By: adenny <adenny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 15:40:03 by adenny            #+#    #+#             */
-/*   Updated: 2024/11/29 15:40:04 by adenny           ###   ########.fr       */
+/*   Updated: 2024/12/20 03:20:18 by adenny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*ft_strchr(char *s, int c)
-{
-	unsigned int	i;
-	char			cc;
-
-	cc = (char)c;
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == cc)
-			return ((char *)&s[i]);
-		i++;
-	}
-	if (s[i] == cc)
-		return ((char *)&s[i]);
-	return (NULL);
-}
-
+// Fonction interne pour remplir la variable `readed` avec les données lues
 static char	*_fill_readed(int fd, char *readed, char *buffer)
 {
 	ssize_t	b_read;
@@ -44,33 +27,40 @@ static char	*_fill_readed(int fd, char *readed, char *buffer)
 			free(readed);
 			return (NULL);
 		}
-		else if (b_read == 0)
-			break ;
 		buffer[b_read] = '\0';
 		if (!readed)
 			readed = ft_strdup("");
+		if (!readed)
+			return (NULL); // Échec de ft_strdup
 		tmp = readed;
 		readed = ft_strjoin(tmp, buffer);
 		free(tmp);
 		tmp = NULL;
+		if (!readed)
+			return (NULL); // Échec de ft_strjoin
 		if (ft_strchr(buffer, '\n'))
 			break ;
 	}
 	return (readed);
 }
 
+// Fonction interne pour extraire la ligne complète et mettre à jour `readed`
 static char	*_set_line(char *line_buffer)
 {
 	char	*left_c;
-	ssize_t	i;
+	size_t	i;
 
 	i = 0;
 	while (line_buffer[i] != '\n' && line_buffer[i] != '\0')
 		i++;
-	if (line_buffer[i] == 0 || line_buffer[1] == 0)
+	if (line_buffer[i] == '\0')
 		return (NULL);
-	left_c = ft_substr(line_buffer, i + 1, ft_strlen(line_buffer) - i);
-	if (*left_c == 0)
+	if (line_buffer[i + 1] == '\0')
+		return (NULL);
+	left_c = ft_substr(line_buffer, i + 1, ft_strlen(line_buffer) - i - 1);
+	if (!left_c)
+		return (NULL);
+	if (*left_c == '\0')
 	{
 		free(left_c);
 		left_c = NULL;
@@ -79,6 +69,7 @@ static char	*_set_line(char *line_buffer)
 	return (left_c);
 }
 
+// Fonction principale pour obtenir la prochaine ligne du fichier
 char	*get_next_line(int fd)
 {
 	char		*line;
@@ -86,21 +77,25 @@ char	*get_next_line(int fd)
 	static char	*readed;
 
 	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	// Vérification du descripteur de fichier et des paramètres
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 	{
 		free(readed);
-		free(buffer);
 		readed = NULL;
-		buffer = NULL;
+		free(buffer);
 		return (NULL);
 	}
-	if (!buffer)
-		return (NULL);
 	line = _fill_readed(fd, readed, buffer);
 	free(buffer);
 	buffer = NULL;
 	if (!line)
+	{
+		free(readed);
+		readed = NULL;
 		return (NULL);
+	}
 	readed = _set_line(line);
 	return (line);
 }
--- Core Curriculum/Rank-01/ft_next_line/get_next_line_utils.c
+++ Core Curriculum/Rank-01/ft_next_line/get_next_line_utils.c
@@ -6,16 +6,30 @@
 /*   By: adenny <adenny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 15:39:59 by adenny            #+#    #+#             */
-/*   Updated: 2024/11/29 15:40:00 by adenny           ###   ########.fr       */
+/*   Updated: 2024/12/20 03:20:21 by adenny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+// Fonction pour calculer la longueur d'une chaîne de caractères
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+// Fonction pour dupliquer une chaîne de caractères
 char	*ft_strdup(char *s1)
 {
-	char			*dest;
-	unsigned int	i;
+	char	*dest;
+	size_t	i;
 
 	dest = (char *)malloc(ft_strlen(s1) + 1);
 	if (!dest)
@@ -26,60 +40,53 @@ char	*ft_strdup(char *s1)
 		dest[i] = s1[i];
 		i++;
 	}
-	dest[i] = 0;
+	dest[i] = '\0';
 	return (dest);
 }
 
-size_t	ft_strlen(char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
+// Fonction pour extraire une sous-chaîne d'une chaîne donnée
 char	*ft_substr(char *s, unsigned int start, size_t len)
 {
 	size_t	i;
+	size_t	s_len;
 	char	*str;
 
 	if (!s)
 		return (NULL);
-	if (start > ft_strlen(s))
-		return (malloc(1));
-	if (len > ft_strlen(s + start))
-		len = ft_strlen(s + start);
-	str = malloc((len + 1) * sizeof(char));
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (len > s_len - start)
+		len = s_len - start;
+	str = (char *)malloc((len + 1) * sizeof(char));
 	if (!str)
 		return (NULL);
 	i = 0;
-	while (i < len)
+	while (i < len && s[start + i])
 	{
 		str[i] = s[start + i];
 		i++;
 	}
-	str[i] = 0;
+	str[i] = '\0';
 	return (str);
 }
 
+// Fonction pour concaténer deux chaînes de caractères
 char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*res;
+	size_t	i;
+	size_t	j;
 
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
 	res = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (!res)
 		return (NULL);
-	fill_str(res, s1, s2);
-	return (res);
-}
-
-void	fill_str(char *res, char *s1, char *s2)
-{
-	unsigned int	i;
-	unsigned int	j;
-
 	i = 0;
 	j = 0;
 	while (s1[j])
@@ -88,4 +95,26 @@ void	fill_str(char *res, char *s1, char *s2)
 	while (s2[j])
 		res[i++] = s2[j++];
 	res[i] = '\0';
+	return (res);
+}
+
+// Fonction pour trouver la première occurrence d'un caractère dans une chaîne
+char	*ft_strchr(char *s, int c)
+{
+	size_t	i;
+	char	cc;
+
+	if (!s)
+		return (NULL);
+	cc = (char)c;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == cc)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == cc)
+		return ((char *)&s[i]);
+	return (NULL);
 }
