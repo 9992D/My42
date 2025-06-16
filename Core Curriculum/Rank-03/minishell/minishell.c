377f4452f7ee0276b656550f168a1be0289bb659
9992D <amaury.denny@icloud.com>
2025-06-16 16:27:48 +0200

    Makefile

--- Core Curriculum/Rank-03/minishell/minishell.c
+++ Core Curriculum/Rank-03/minishell/minishell.c
@@ -9,26 +9,33 @@ void print_tokens(t_token *token_list)
 	}
 }
 
-int main(int ac, char **av)
+int main(void)
 {
-	if (ac != 2)
-	{
-		fprintf(stderr, "Usage: %s \"command line\"\n", av[0]);
-		return 1;
-	}
-	t_character *char_list = build_token_list(av[1]);
-	if (!char_list)
-	{
-		fprintf(stderr, "Error building character list.\n");
-		return 1;
-	}
-	t_token *token_list = convert_to_tokens(char_list);
-	if (!token_list)
-	{
-		fprintf(stderr, "Error converting to tokens.\n");
-		return 1;
-	}
-	print_tokens(token_list);
-	free_token_list(token_list);
-	return 0;
+    char *input = readline("minishell> ");
+    if (!input)
+    {
+        fprintf(stderr, "Error reading input.\n");
+        return 1;
+    }
+
+    t_character *char_list = build_token_list(input);
+    if (!char_list)
+    {
+        fprintf(stderr, "Error building character list.\n");
+        free(input);
+        return 1;
+    }
+
+    t_token *token_list = convert_to_tokens(char_list);
+    if (!token_list)
+    {
+        fprintf(stderr, "Error converting to tokens.\n");
+        free(input);
+        return 1;
+    }
+
+    print_tokens(token_list);
+    free_token_list(token_list);
+    free(input);
+    return 0;
 }
